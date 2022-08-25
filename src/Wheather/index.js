69e23149891d6c9d95be1093ebb389f23151e55d
rafal <r.feliga@hotmail.co.uk>
2022-08-25 06:30:13 +0200

    updating Weather based on new provider

--- src/Wheather/index.js
+++ src/Wheather/index.js
@@ -1,37 +1,29 @@
 import Grid from '../Layout/Grid'
 import Container from '../Layout/Container'
-import { useEffect } from 'react'
 import Tile from './Tile'
-import { useWeather } from './context/provider'
+import useWeather from './context/useWeather'
 
 export default function Weather(){
 
   const [state, setState] = useWeather()
 
-
   const innerGrid = {
-    '&>:first-child': {
+    '&>div:first-of-type': {
       borderRadius: '5px 0 0 5px',
     },
-    '&>:last-child': {
+    '&>div:last-of-type': {
       borderRadius: '0 5px 5px 0',
     },
   }
 
-  useEffect(()=>{
-    if(!state.isLoading){
-      setState.find()
-    }
-  }, [])
+  const columnsCount = state.list.length || 1
 
-
-
-  return <Grid columns={1} rows={2}>
+  return <Grid columns={1} rows={2} style={{justifyItems:'center'}}>
     <Container style={{gridColumn: '1 / -1', display: 'flex', flexDirection:'row'}}>
-      <input value={state.city} onChange={setState.change}/>
-      <button onClick={setState.find}>Find</button>
+      <input type={'text'} value={state.city} onChange={setState.change}/>
+      <button name={"find"} onClick={setState.find}>Find</button>
     </Container>
-    <Grid columns={4} rows={1} style={innerGrid}>
+    <Grid columns={'auto-fit'} columnWidth={`minmax(100px, calc(95%/${columnsCount}))`} columnGap={'5px'} rows={1} style={innerGrid}>
       {state.list ? state.list.map((item,index)=><Tile key={'weather-tile'+index} item={item}/>) : null}
     </Grid>
   </Grid>
--- src/Wheather/utils.js
+++ src/Wheather/utils.js
@@ -7,15 +7,25 @@ export function getDay(date=''){
   const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   const d = new Date(date)
 
-  return `${days[d.getDay()]} ${d.getDate()}${ordinals(d.getDate())}`;
+  if (isNaN(d.getDay())) {
+    throw new Error('Incorrect Date Format: ' + toString(date))
+  }
+
+  return `${days[d.getDay()]} ${ordinals(d.getDate())}`;
 }
 
 export function ordinals(d) {
-  if (d > 3 && d < 21) return 'th';
+  if ('number' !== typeof d ) return undefined
+  if (d > 3 && d < 21) return d+'th';
   switch (d % 10) {
-    case 1:  return "st";
-    case 2:  return "nd";
-    case 3:  return "rd";
-    default: return "th";
+    case 1:  return d+"st";
+    case 2:  return d+"nd";
+    case 3:  return d+"rd";
+    default: return d+"th";
   }
+}
+
+export function getCityFromParams(){
+  const params = new URL(window.location)
+  return params.searchParams.get('cityName')
 }

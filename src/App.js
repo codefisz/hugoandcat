248ddcdd31a895c8d56780450ef5f4d2d452bd7e
rafal <r.feliga@hotmail.co.uk>
2022-08-25 06:30:58 +0200

    updating App components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import './App.css';
 import Weather from './Wheather'
 import WeatherWrapper from './Wheather/context/provider'
+import Grid from './Layout/Grid'
+import { getCityFromParams } from './Wheather/utils'
 
 function App() {
+  const city = getCityFromParams()
   return (
     <div className="App">
       <header className="App-header">
-        <WeatherWrapper city={'London,UK'}>
-          <Weather/>
-        </WeatherWrapper>
-        <WeatherWrapper city={'Palma'}>
-          <Weather/>
-        </WeatherWrapper>
+        <Grid columns={2} rows={3} width={'80%'} style={{justifyItems: 'center'}}>
+          <WeatherWrapper city={city}>
+            <Weather/>
+          </WeatherWrapper>
+          <WeatherWrapper city={'Palermo'}>
+            <Weather/>
+          </WeatherWrapper>
+        </Grid>
       </header>
     </div>
   );

5c6824961c38ccaeae69a3219937e353cbdeebfb
rafal <r.feliga@hotmail.co.uk>
2022-08-25 06:30:35 +0200

    updating Tile

--- src/Wheather/Tile/index.js
+++ src/Wheather/Tile/index.js
@@ -9,13 +9,13 @@ export default function Tile({item}){
   const loader = new IconLoader()
   return <div >
     <Container style={{position: 'relative'}} className={`${style['Tile--default']} ${style['Tile--'+ item.weather[0].description.toLowerCase().replace(/\s/g, "-")]}`}>
-      <Column rows={1} rowHeight={'auto 2fr auto 1fr auto 2fr'}>
+      <Column >
         <div className={style['Tile__day']}>{getDay(item.dt_txt)}</div>
         <div>{ loader.get(item.weather[0].description.toLowerCase().replace(/\s/g, ""))}</div>
-        <div className={style['Tile__desc']}>{item.weather[0].description}</div>
+        <div className={`${style['Tile__desc']}  ${style['no-mobile']}`}>{item.weather[0].description}</div>
         <div className={style['Tile__main']}>{Math.round(item.main.temp)}<span>&#8451;</span></div>
-        <div className={style['Tile__secondary']}>{toFahrenheit( item.main.temp)}<span>&#8457;</span></div>
-        <div className={style['Tile__shadow']}>{ loader.get(item.weather[0].description.toLowerCase().replace(/\s/g, ""))}</div>
+        <div className={`${style['Tile__secondary']} ${style['no-mobile']}`}>{toFahrenheit( item.main.temp)}<span>&#8457;</span></div>
+        <div className={`${style['Tile__shadow']} ${style['no-mobile']}`}>{ loader.get(item.weather[0].description.toLowerCase().replace(/\s/g, ""))}</div>
       </Column>
     </Container>
   </div>
